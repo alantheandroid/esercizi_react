29ab1413e559ef89bc5bd52ba22f4cda338f5df7
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-08-02 20:49:53 +0200

    feat: move "TodoList" component render logic to render prop

--- src/index.js
+++ src/index.js
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { createRef } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-class Container extends React.Component {
+class TodoList extends React.Component {
+  state = { items: ["navbar", "hero-section", "cards", "footer"], newItem: "" };
+
+  clearList = () => {
+    this.setState({
+      items: [],
+    });
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      newItem: event.target.value,
+    });
+  };
+
+  addListItem = () => {
+    this.setState({
+      items: [...this.state.items, this.state.newItem],
+      newItem: "",
+    });
+  };
+
+  removeListItem = (removedItem) => {
+    const filteredArray = this.state.items.filter(
+      (item) => item !== removedItem
+    );
+
+    this.setState({
+      items: [...filteredArray],
+    });
+  };
+
   render() {
     return (
       <div className="container">
-        <header>{this.props.title}</header>
-        <article>{this.props.children}</article>
+        <fieldset>
+          {this.props.render(this.state.items, this.removeListItem)}
+          <button name="reset" onClick={this.clearList}>
+            Reset ❌
+          </button>
+        </fieldset>
+        <label>
+          <input
+            type={"text"}
+            value={this.state.newItem}
+            onChange={this.handleInputChange}
+            autoFocus
+          ></input>
+          <button
+            name="add"
+            onClick={this.addListItem}
+            disabled={!this.state.newItem}
+          >
+            add ➕
+          </button>
+        </label>
       </div>
     );
   }
@@ -18,14 +68,27 @@ class Container extends React.Component {
 class App extends React.Component {
   render() {
     return (
-      <Container title={<h2>Lorem ipsum</h2>}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam ut
-          rem, natus sequi molestiae pariatur doloremque distinctio qui
-          cupiditate tenetur error suscipit exercitationem velit voluptas illum
-          sit corrupti? Voluptas, reprehenderit.
-        </p>
-      </Container>
+      <TodoList
+        render={(items, removeFunction) => {
+          const todo = items.map((item) => (
+            <li key={item.toString()}>
+              <p>
+                {item}
+                <button
+                  name="remove"
+                  onClick={() => {
+                    removeFunction(item);
+                  }}
+                >
+                  remove
+                </button>
+              </p>
+            </li>
+          ));
+
+          return <ul>{todo}</ul>;
+        }}
+      ></TodoList>
     );
   }
 }
